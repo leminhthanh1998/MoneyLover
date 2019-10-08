65bcbde93b15f42bfae6e5fab55ec4dbc47b925d
BICH <nnb963@gmail.com>
2019-10-08 23:41:26 +0700

    add giao dien

---
+++ MoneyLover/DangKy.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyLover
+{
+    /// <summary>
+    /// Interaction logic for DangKy.xaml
+    /// </summary>
+    public partial class DangKy : Window
+    {
+        public DangKy()
+        {
+            InitializeComponent();
+        }
+    }
+}
